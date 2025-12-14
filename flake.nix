edd62a2884f499d43e9dc5e9e327cdc2782f175d
kodi <kodicw@gmail.com>
2025-12-14 00:46:23 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A Rust application packaged with Nix flakes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      eachSystem = nixpkgs.lib.genAttrs supportedSystems;
+
+      pkgs = eachSystem (
+        system:
+        import nixpkgs {
+          inherit system;
+        }
+      );
+
+    in
+    {
+      packages = eachSystem (system: {
+        default = pkgs.${system}.rustPlatform.buildRustPackage rec {
+          pname = "bowser-rs"; # Replace with your app name
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/${pname} $out/bin/
+          '';
+        };
+      });
+    };
+}
